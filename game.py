90cea0fd1c358c905b336822f3310045e87e25d7
Tuukka <tuukka.tarkkinen@skillio.ai>
2025-01-14 10:18:35 +0200

    Program start and main loop

--- game.py
+++ game.py
@@ -1,1 +1,57 @@
+def main ():
+    while True:
+        print("Welcome to quiz appliaction!")
+        choice = input("Would you like to play? y/n ")
+        if choice in ("y", "yes", "Yes", "Y"):
+            score = quiz_1()
+            newgame = end(score)
+            if newgame == 0
+                pass
+            else:
+                print("Thanks. bye!")
+                break
+                
+        elif choice in ("n", "no", "No", "N"):
+            print("Good bye!")
+            break
+        else: 
+            pass
 
+        
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+# While true loopissa
+    # Alku (haluatko pelata)
+
+
+    1.
+        # näytä kysymys (print)
+        # promptaa vastaus
+        # score counter +=
+
+    2.
+        # näytä kysymys (print)
+        # promptaa vastaus 
+        # score counter +=
+
+    3.
+        # näytä kysymys (print)
+        # promptaa vastaus
+        # score counter +=
+    
+    # return score
+    
+
+    # End
+    # Print score to player
+    # Play again
