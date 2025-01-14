b0c3e8d35ea251e6394e7537cf23820d6d717bab
samusyvaoja <samu.syv@gmail.com>
2025-01-14 10:18:49 +0200

    Samu's quiz function

--- game.py
+++ game.py
@@ -1,1 +1,26 @@
+#Tuukan alku
 
+def quiz_1():
+    score = 0
+    print("Welcome to the quiz! Please give the answer in format yyyy, E.g: 1600")
+    answers = [1300,1800,1800]
+    q1 = "In which century did the Hundred Years' War between England and France begin?"
+    q2 = "In which century did the American Civil War take place?"
+    q3 = "In which century did the Napoleonic Wars occur?"
+    questions = [q1,q2,q3]
+    index = 0
+    for question in questions:
+        print(question)
+        try:
+            answer = int(input("Answer: "))
+            if answer == answers[index]:
+                print("Correct!")
+                score += 1
+            else:
+                print("Incorrect!")
+            index += 1
+        except ValueError:
+            print("Wrong format!")
+    return score
+
+#Lindan loppu
