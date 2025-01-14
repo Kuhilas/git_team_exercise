6e9354783319b6f415e440b98d68b6fa67a238d6
liulma <linda.ulma@skillio.ai>
2025-01-14 10:18:59 +0200

    Added fuction end

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
+def end(score):
+    print(f"Your score was: {score} points")
+    play_again = input("Do you want to play again? ")
 
+    if play_again in ("y", "yes", "Yes", "Y"):
+        return 0
+    else:
+        return 1
